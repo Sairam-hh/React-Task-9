a35c85bcfaa4b440a22d29cafc7e311c78a145be
Banti sairam <163281250+Sairam-hh@users.noreply.github.com>
2026-04-06 22:11:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./App.css"
+import StudentCard from './studentCard'
+import RegistrationForm from './RegistrationForm'
+import Demo from './Demo'
+import Timer from './Components/StopWatch'
+import StopWatch from './Components/StopWatch'
+import UseEffectApi from './Components/UseEffectApi'
+import ChatBox from './Components/ChatBox'
+import UseRefHook from './Components/UseRefHook'
+import Parent from './Context/Parent'
+
+export default function App() {
+  
+
+  return (
+    <div>
+      <Parent/>
+      
+      
+      
+      
+      {/* <UseRefHook/> */}
+      
+
+
+
+    </div>
+  )
+}
